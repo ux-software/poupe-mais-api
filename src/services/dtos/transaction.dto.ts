4af97cc6160dfb36d8aa16ef6c266bf9fa55e039
Neto Sousa <netosouzacp@gmail.com>
2024-04-07 21:36:24 -0300

    fea: add transaction dto

--- src/services/dtos/transaction.dto.ts
+++ src/services/dtos/transaction.dto.ts
@@ -23,10 +23,46 @@ export class CreateTransactionInput {
   type!: (typeof TRANSACTION_TYPE)[number];
 
   @IsUUID('4', { message: 'Id da categoria inválida' })
-  categoryId?: string;
+  @ValidateIf((object, value) => value !== null)
+  categoryId?: string | null;
 
+  @IsUUID('4', { message: 'Id do usuário inválido' })
+  @IsString({ message: 'Id do usuário inválido' })
+  userId!: string;
+}
+
+export class CreateTransactionBody {
+  // @IsNumber({ message: 'Valor da transação inválido' })
+  @IsNotEmpty({ message: 'Valor da transação é obrigatório' })
+  value!: number;
+
+  @IsString({ message: 'Descrição da transação inválida' })
+  @ValidateIf((_, value) => value !== undefined)
+  description?: string;
+
+  @IsIn(TRANSACTION_TYPE, {
+    message: 'Tipo da transação deve ser "EXPENSE" ou "INCOME"',
+  })
+  type!: (typeof TRANSACTION_TYPE)[number];
+
+  @IsUUID('4', { message: 'Id da categoria inválida' })
+  @ValidateIf((_: { type: 'EXPENSE' | 'INCOME' }) => {
+    if (_.type === 'EXPENSE') {
+      return false;
+    }
+    return true;
+  })
+  categoryId?: string | null;
+}
+
+export class DeleteTransactionInput {
   @IsUUID('4', { message: 'Id do usuário inválido' })
   @IsString({ message: 'Id do usuário inválido' })
   @IsNotEmpty({ message: 'Id do usuário é obrigatório' })
   userId!: string;
+
+  @IsUUID('4', { message: 'Id da transação inválido' })
+  @IsString({ message: 'Id da transação inválido' })
+  @IsNotEmpty({ message: 'Id da transação é obrigatório' })
+  transactionId!: string;
 }
